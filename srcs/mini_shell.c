17b9706a2ee8737f8c41e7f9a85b42387a1e78ca
Younes Anflous <anayouness51@gmail.com>
2025-03-06 01:46:09 +0000

    update

--- srcs/mini_shell.c
+++ srcs/mini_shell.c
@@ -1,4 +1,5 @@
-#include "mini_shell.h"
+# include "mini_shell.h"
+# include "../parsing/tokenizer.h"
 
 int main(int argc, char **argv, char **env)
 {
@@ -15,6 +16,7 @@ int main(int argc, char **argv, char **env)
             input = readline("minishell$ "); // prompt && take input 
             if (input == NULL)
                 break;
+            // parser
 
             // >>> build-ins.
             if (ft_strncmp(input, "echo", 4) == 0)
