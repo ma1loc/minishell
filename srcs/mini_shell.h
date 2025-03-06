b6bb476f54bc05bd0bc71724e2b7c86fceb3410c
Younes Anflous <anayouness51@gmail.com>
2025-03-06 01:33:45 +0000

    update

--- srcs/mini_shell.h
+++ srcs/mini_shell.h
@@ -21,13 +21,9 @@ int		ft_strncmp(char *s1, char *s2, size_t n);
 char	*ft_strdup(char *s1);
 void	ft_putstr_fd(char *s, int fd);
 int     ft_strcmp(char *s1, char *s2);
+int     ft_isnum(int c);
 
-// >>> built_in_cmds
-void	echo_cmd(char *input);              // the echo command fun. [done]
-void    execute_echo(char *input, int newline);
-int     cd_cmd(char *args);                 // the cd command fun. [done]
-void	pwd_cmd();                          // the pwd path print fun. [done]
-void	exit_cmd();              // the exit command fun.
+
 
 // >>> sig
 void	signals(int signal);
@@ -44,12 +40,21 @@ typedef struct s_env
     struct  s_env *next;
 } t_env;
 
-t_env   *init_env(char **env);
-void    print_env(t_env *env_list);
 t_env	*ft_lstlast(t_env *lst);
 void	ft_lstadd_back(t_env **lst, t_env *new);
-void	unset_env(t_env **env_list, char *key);
-void	env_cmd(char **env);
 
+// >>> built_in_cmds
+void	echo_cmd(char *input);              // the echo command fun. [done]
+void    execute_echo(char *input, int newline);
+int     cd_cmd(char *args);                 // the cd command fun. [done]
+void	pwd_cmd();                          // the pwd path print fun. [done]
+void	exit_cmd();              // the exit command fun.
+
+// >>> env command
+t_env   *init_env(char **env);
+void	env_cmd(t_env *env_list);
+
+// >>> unset command
+void	unset_cmd(t_env **env_list, char *key);
 
 # endif
