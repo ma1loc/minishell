9768134b52bf3e3bef4aa01d172b7b9146c36e20
Younes Anflous <anayouness51@gmail.com>
2025-03-07 17:59:53 +0000

    update

--- execution/built-ins/exit_cmd.c
+++ execution/built-ins/exit_cmd.c
@@ -13,3 +13,47 @@
 // 	}
 
 // }
+
+// void cd(t_set_env *built_in)
+// {
+//     int status;
+//     char *home;
+
+//     if (built_in->cmd->args[1] == NULL)
+//     {
+//         home = getenv("HOME");
+//         if (!home)
+//         {
+//             fprintf(stderr, "cd: 'HOME' not set\n");
+//             return;
+//         }
+//         status = chdir(home);
+//         if (status == -1)
+//             perror("cd");
+//     }
+//     else
+//     {
+//         status = chdir(built_in->cmd->args[1]);
+//         if (status == -1)
+//             perror("cd");
+//     }
+// }
+
+// void get_pwd(t_set_env *built_in)
+// {
+//     char buf_path[PATH_MAX];
+//     char *path;
+
+//     path = getcwd(buf_path, sizeof(buf_path));
+//     if (path)
+//         built_in->pwd = strdup(path);
+//     else
+//         perror("pwd");
+// }
+
+// void cd_cmd(t_set_env *built_in)
+// {
+//     cd(built_in);
+//     free(built_in->pwd);
+//     get_pwd(built_in);
+// }
