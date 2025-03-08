36cd498a21a0576a27af86f06a267a1a2fb44521
joseph <josephtabia1@gmail.com>
2025-03-08 01:42:07 +0000

    merge

---
+++ execution/built-ins/unset_cmd.c
@@ -0,0 +1,29 @@
+#include "../../srcs/mini_shell.h"
+
+// >>> reomve the key, value from the env.
+void	unset_env(t_env **env_list, char *key)
+{
+    t_env	*current;
+    t_env	*previous;
+
+    if (!*env_list)
+		return;
+    current = *env_list;
+    previous = NULL;
+    while (current)
+    {
+        if (ft_strcmp(current->key, key) == 0)
+        {
+            if (previous == NULL)
+                *env_list = current->next;
+            else
+                previous->next = current->next;
+            free(current->key);
+            free(current->value);
+            free(current);
+            return;
+        }
+        previous = current;
+        current = current->next;
+    }
+}
