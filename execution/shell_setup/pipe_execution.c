161128a84188dcd7fb4d28195a010c8d713940d0
Younes Anflous <anayouness51@gmail.com>
2025-03-18 23:09:38 +0000

    strucute the code

--- execution/shell_setup/pipe_execution.c
+++ execution/shell_setup/pipe_execution.c
@@ -4,7 +4,8 @@ void    set_pipe(t_setup *setup, int *fd)
 {
     if (pipe(fd) == -1)
     {
-        ft_perror(setup, "Error: pipe failed\n", FAIL);    // >>> set the exit status
+        // >>> set the exit status
+        ft_perror(setup, "Error: pipe failed\n", FAIL);
         return;
     }
 }
@@ -35,53 +36,41 @@ pid_t    set_second_fork(t_setup  *setup, pid_t pid_1, int *fd)
         perror("fork");
         close(fd[0]);
         close(fd[1]);
-        waitpid(pid_1, NULL, 0);  // wait for first child
+        waitpid(pid_1, NULL, 0);  // >>> wait for first child
         ft_perror(setup, NULL, FAIL);
         return (-1);
     }
     return (pid_2);
 }
 
-void execute_pipe(t_tree *tree, t_setup *setup)
+void    first_child_process(t_setup *setup, t_tree *tree, int *fd)
 {
-    int fd[2];
-    pid_t pid_1;
-    pid_t pid_2;
-    int status;
-    
-    set_pipe(setup, fd);    // >>> seting the pipe
-    pid_1 = set_first_fork(setup, fd); // >>> first child process left side
-    if (pid_1 == 0)
+    // >>> child process - execute left command
+    // >>> close read end, redirect stdout to pipe write end
+    close(fd[0]);
+    if (dup2(fd[1], STDOUT_FILENO) == -1)
     {
-        // >>> child process - execute left command
-        // >>> close read end, redirect stdout to pipe write end
-        close(fd[0]);
-        if (dup2(fd[1], STDOUT_FILENO) == -1)
-        {
-            perror("dup2");
-            ft_perror(setup, NULL, FAIL);
-            exit(FAIL);
-        }
-        close(fd[1]);
-        if (tree->left)
-        {
-            if (tree->left->type == TOKEN_WORD)
-            {
-                setup->cmd = tree->left->cmd;
-                execute_command(tree->left, setup);
-            }
-            else
-                execution(tree->left, setup);
-        }
-        exit(EXIT_SUCCESS);
+        perror("dup2");
+        ft_perror(setup, NULL, FAIL);
+        exit(FAIL);
     }
-    
-    // >>> second child process right side    
-    pid_2 = set_second_fork(setup, pid_1, fd);
-    
-    if (pid_2 == 0)
+    close(fd[1]);
+    if (tree->left)
     {
-        // >>> child process - execute right command
+        if (tree->left->type == TOKEN_WORD)
+        {
+            setup->cmd = tree->left->cmd;
+            execute_command(tree->left, setup);
+        }
+        else
+            execution(tree->left, setup);
+    }
+    exit(EXIT_SUCCESS);
+}
+
+void    second_child_process(t_setup *setup, t_tree *tree, int *fd)
+{
+        // >>> child process execute right command
         // >>> close write end, redirect stdin to pipe read end
         close(fd[1]);
         if (dup2(fd[0], STDIN_FILENO) == -1)
@@ -103,12 +92,28 @@ void execute_pipe(t_tree *tree, t_setup *setup)
                 execution(tree->right, setup);
         }
         exit(EXIT_SUCCESS);
-    }
+}
+
+void    execute_pipe(t_tree *tree, t_setup *setup)
+{
+    int fd[2];
+    pid_t pid_1;
+    pid_t pid_2;
+    int status;
     
+    // >>> seting the pipe
+    set_pipe(setup, fd);
+    // >>> first child process left side
+    pid_1 = set_first_fork(setup, fd);
+    if (pid_1 == 0)
+        first_child_process(setup, tree, fd);
+    // >>> second child process right side
+    pid_2 = set_second_fork(setup, pid_1, fd);
+    if (pid_2 == 0)
+        second_child_process(setup, tree, fd);
     // >>> parent process
     close(fd[0]);
     close(fd[1]);
-    
     // >>> wait for both children to finish
     waitpid(pid_1, &status, 0);
     waitpid(pid_2, &status, 0);
--- srcs/mini_shell.c
+++ srcs/mini_shell.c
@@ -4,30 +4,30 @@
 int		main(int argc, char **argv, char **env)
 {
     (void)argv;
-    t_setup  *setup_env;
+    t_setup  *setup;
 
-    setup_env = NULL;
+    setup = NULL;
     if (argc == 1)
     {
         while (true)
         {
-            setup_env = shell_env_setup(env);
-            setup_env->input = readline("minishell$ ");
-            if (setup_env->input == NULL)
+            setup = shell_env_setup(env);
+            setup->input = readline("minishell$ ");
+            if (setup->input == NULL)
                 break ;  // >>> for segnal case to free litter on
-            if (setup_env->input[0] == '\0')
+            if (setup->input[0] == '\0')
                 continue ;
-            setup_env->token = tokenize(setup_env->input);
-            if (!setup_env->token)
+            setup->token = tokenize(setup->input);
+            if (!setup->token)
                 exit(1);
-            setup_env->cmd = pars_tokens(setup_env->token);
-            setup_env->tree = build_tree_commande(setup_env->cmd);
-            execution(setup_env->tree, setup_env);
+            setup->cmd = pars_tokens(setup->token);
+            setup->tree = build_tree_commande(setup->cmd);
+            execution(setup->tree, setup);
 
-			add_history(setup_env->input);
+			add_history(setup->input);
         }
     }
     else
-        ft_perror(setup_env, "No extra args, please ;)\n", 1); // exit status litter on
+        ft_perror(setup, "No extra args, please ;)\n", 1); // exit status litter on
     return 0;
 }
