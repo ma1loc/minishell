d8ea6ed8d883761a1fac8d0e813ebcfb30a073e2
Younes Anflous <anayouness51@gmail.com>
2025-03-26 01:04:25 +0000

    add some noting

--- srcs/mini_shell.c
+++ srcs/mini_shell.c
@@ -20,13 +20,15 @@ int		main(int argc, char **argv, char **env)
             break ;  // >>> for segnal case to free litter on
         if (setup->input[0] == '\0')
             continue ;
-        setup->token = tokenize(setup->input);
+        setup->token = tokenize(setup->input, setup);
         if (!setup->token)
-            exit(EXIT_FAILURE);
+			continue ;
+            // exit(EXIT_FAILURE);
         setup->cmd = pars_tokens(setup->token);
         setup->tree = build_tree_commande(setup->cmd);
         execution(setup->tree, setup);
 		add_history(setup->input);
+		free(setup->input);
     }
     return 0;
 }
